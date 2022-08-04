359a81a9728306f3b59700fec55f54eff92feb2d
ERIK M. DODD <emdodd27@gmail.com>
2022-08-04 13:11:50 -0700

    Javascript functions were added

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-let person = prompt("Please enter your name");
-if (person != null) {
-    document.getElementById("greeting").innerHTML =
-    "Hello " + person + "! How are you today?";
-} 
-
-console.log(person)
-
-let text;
-if (confirm("Looking for fun activities in Seattle?") == true) {
-text = "Awesome! We can help you!";
-} else {
-    text = "No worries! We can still help you!";
+function yourName() {
+    let person = prompt("Please enter your name");
+    while(person == ''){
+        person = ("Please enter your name")
+    }
+    if (person == "Erik"){
+        document.getElementById("greeting").innerHTML =
+        "Way to Go " + person + "!";
+    }
+     else {
+        document.getElementById("greeting").innerHTML =
+        "Hello " + person + "! How are you today?";
+    } 
 }
+
+function yourActivities() {
+    let text;
+    if (confirm("Looking for fun activities in Seattle?") == true) {
+    text = "Awesome! We can help you!";
+    } else {
+        text = "No worries! We can still help you!";
+    }    
+}
+yourName();
+yourActivities();
